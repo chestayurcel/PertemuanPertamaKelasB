df21075517a7622829329d6708c089ff32120102
Yurcell <chestayurcel@gmail.com>
2024-02-26 15:25:51 +0700

    display masukkan panjang

--- PertemuanPertamaKelasB/PertemuanPertamaKelasB.cpp
+++ PertemuanPertamaKelasB/PertemuanPertamaKelasB.cpp
@@ -13,11 +13,6 @@ int main()
     
     int nPanjang, nLebar, nLuas;
     cout << "Masukkan Panjang = ";
-    cin >> nPanjang;
-    cout << "Masukkan Lebar = ";
-    cin >> nLebar;
-    nLuas = nPanjang * nLebar;
-    cout << "Luasnya adalah = " << nLuas << endl;
 
     system("pause");
 
